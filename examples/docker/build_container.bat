7d4c9bb7ccb5f0c7aad9ad9acf64aa976fc7f38c
Dani Rey <dani.rey.ch@gmail.com>
2019-07-30 17:37:46 +0200

    Update docker scalawebtest/sbt docker image for release 3.0.1

--- examples/docker/build_container.bat
+++ examples/docker/build_container.bat
@@ -1,2 +1,2 @@
 cd C:\Users\daniel.rey\git\ScalaWebTest\examples\docker
-docker run -it --rm -v %CD%\src\test\scala:/home/sbtuser/src/test/scala scalawebtest/sbt:3.0.0
+docker run -it --rm -v %CD%\src\test\scala:/home/sbtuser/src/test/scala scalawebtest/sbt:3.0.1
